1be6595cd1b65a670425bf90a4d2af89b9defbc0
Bruno Saibert <brunosaibert@gmail.com>
2022-11-17 19:58:14 -0300

    fix: adiciona link no card

--- src/components/CardPokemon.tsx
+++ src/components/CardPokemon.tsx
@@ -17,22 +17,22 @@ export type CardPokemonProps = {
 
 function CardPokemon(props: CardPokemonProps) {
   return (
-    // <Link to={`/details/${props.id}`}>
-    <Card className={`type--${props.types[0].type.name.toLowerCase()}`}>
-      <div>
-        <Number>#{String(props.id).padStart(3, "0")}</Number>
-        <Title>{props.name}</Title>
+    <Link to={`/details/${props.id}`}>
+      <Card className={`type--${props.types[0].type.name.toLowerCase()}`}>
+        <div>
+          <Number>#{String(props.id).padStart(3, "0")}</Number>
+          <Title>{props.name}</Title>
 
-        {props.types.map((item, index) => {
-          return <Badge key={index} name={item.type.name} />;
-        })}
-      </div>
-      <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`}
-        alt={props.name}
-      />
-    </Card>
-    // </Link>
+          {props.types.map((item, index) => {
+            return <Badge key={index} name={item.type.name} />;
+          })}
+        </div>
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`}
+          alt={props.name}
+        />
+      </Card>
+    </Link>
   );
 }
 
